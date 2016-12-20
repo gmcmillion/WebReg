b9dfc9c69e293be1c768a93108766fd996619b53
Gregg M <gmcmillion215@gmail.com>
2016-12-19 21:37:52 -0800

    Initial Commit

---
+++ WebReg2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WebReg2
+//
+//  Created by Gregg McMillion on 12/19/16.
+//  Copyright © 2016 Gregg McMillion. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
