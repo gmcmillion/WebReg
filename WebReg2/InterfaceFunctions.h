c5d805f11d8bf219c55eb1bd45d45bbc4c6623b3
John J <jejuarez88@gmail.com>
2016-12-27 01:42:50 -0800

    Refactored and cleaned up code
    Cleaned up main, and cleaned up some code. Still needs some work though.
    
    Fixed a bug when logging out and relogging in that would cause a crash.
    
    Also discovered a bug with the enrollment and disenrollment functions.
    If you enroll in a course then immediately try to disenroll the course
    will not be found. You need to exit the program then reopen it for it to
    work.

--- WebReg2/InterfaceFunctions.h
+++ WebReg2/InterfaceFunctions.h
@@ -2,15 +2,27 @@
 #define INTERFACEFUNCTIONS_HPP 
 
 #include "Student.h"
+#include <vector>
+#include <map>
 
-//Function Prototypes
+//display user menu after they login
 char menu();
-char studentCreationMenu();
+
+//display login menu
 char startUpMenu();
 
-Student createStudent(Student& x);
+//loop that handles menu switch statement
 void menuLoop(Student* x);
 
-//Student createStudent();
+//load all file names into vector
+void setUpUserVector(vector<string>& userNameList);
 
+//load all key value pairs into map
+map<string, Student*> setUpMap(vector<string>& userNameList, map<string, Student*> testMap);
+
+//some function that calls the student's setters
+void addValuesToObject(Student* x, string& userLogin, string& userPW, string& userName, string& userMaxUnits, string& userCU, string& userID);
+
+//this gets user login and password and verifies authentication
+void getLogin(vector<string>& userNameList, map<string, Student*> testMap);
 #endif // !
--- WebReg2/main.cpp
+++ WebReg2/main.cpp
@@ -1,16 +1,7 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <fstream>
-#include <vector>
-#include "Course.h"
-#include "CourseList.h"
-#include "DeptCourse.h"
 #include "Student.h"
 #include "InterfaceFunctions.h"
 
 using namespace std;
-
 
 int main()
 {
@@ -20,168 +11,33 @@ int main()
 	map<string, Student*> testMap;
 	vector<string> userNameList;
 
+	setUpUserVector(userNameList);
+	testMap = setUpMap(userNameList, testMap);
 
-	ifstream inputFile("users.txt");      //Input file
-
-	while (true) {
-
-		string test;
-		inputFile >> test;
-		userNameList.push_back(test);
-
-		if (inputFile.eof()) {
-			break;
-			}
-		}
-
-	inputFile.close();
-
-	for (auto i : userNameList){				// access by value, the type of i is int
-
-		cout << "Vector file is: " << i << endl;
-
-		ifstream inputFile(i + ".txt");			//Input file
-		string userLogin, userPW, userName, userMaxUnits, userCU, userID;
-
-		string
-			courseName, courseNum, type, section,
-			units, professor, time, location,
-			finalDateTime, maxCapacity, currentEnrollment,
-			waitListed, status, emptySpace, name;
-
-		Student* x = new Student;	// <--- make sure to check for mem leaks & deallocate
-
-		while (inputFile) {
-			bool gotMemberData = false;
-			
-			if (!gotMemberData) {
-				getline(inputFile, userLogin);
-				getline(inputFile, userPW);
-				getline(inputFile, userName);
-				getline(inputFile, userID);
-				getline(inputFile, userMaxUnits);
-				getline(inputFile, userCU);
-
-				gotMemberData = true;
-			}
-			cout << userLogin << userPW << userName << userID << userMaxUnits << userCU << endl;
-
-            x->setUserLogin(userLogin);
-			x->setPassCode(userPW);
-			x->setName(userName);
-			x->setID(atoi(userID.c_str()));
-
-			//add student schedule write loop here
-			while (true && !inputFile.eof())
-			{
-				getline(inputFile, courseName);
-				getline(inputFile, courseNum);
-				getline(inputFile, type);
-				getline(inputFile, section);
-				getline(inputFile, units);
-				getline(inputFile, professor);
-				getline(inputFile, time);
-				getline(inputFile, location);
-				getline(inputFile, finalDateTime);
-				getline(inputFile, maxCapacity);
-				getline(inputFile, currentEnrollment);
-				getline(inputFile, waitListed);
-				getline(inputFile, status);
-
-				//this mitigates the condition where a course does not have a final
-				//in that case, we backwards initialize string values to match
-				//the order in which they were read from the file
-				if (finalDateTime.size() < 6)
-				{
-					status = waitListed;
-					waitListed = currentEnrollment;
-					currentEnrollment = maxCapacity;
-					maxCapacity = finalDateTime;
-					finalDateTime = "";
-				}
-				else
-				{
-					getline(inputFile, emptySpace);	//To skip the extra line in txt file (find a better method maybe?)
-				}
-
-				DeptCourse d(
-					professor, courseNum, courseName, type, section,
-					units, time, location, finalDateTime, maxCapacity,
-					currentEnrollment, waitListed, status);				//Create DeptCourse object
-
-				x->studentSchedule.add(&d);	                                //Add DeptCourse to the CourseList
-
-				if (inputFile.eof()) break; // if the file is the last line
-
-			}
-
-			if (inputFile.eof()) {
-				break;
-			}
-		}
-
-		testMap[userPW] = x;				//map object 
-		inputFile.close();
-	}
-
-	//Get choice from welcome menu
 	cout << "\nWelcome to UCI WebReg\n" << endl;
 
-	while (!quitProgram) {
+	while (!quitProgram) 
+	{
 
-		studentOption = studentCreationMenu();
+		studentOption = startUpMenu();
 		cin.ignore();
 
 		switch (studentOption)
 		{
 		case 'A':
-		case 'a': {
-
-			//Variables
-			string loginName, passWord;
-			cout << "Enter username: ";
-			getline(cin, loginName);
-			cout << "Enter password: ";
-			getline(cin, passWord);
-
-			for (auto vectorIterator: userNameList) {
-				
-				cout << vectorIterator << endl;
-
-				/*
-				check if loginName is part of the student accounts txt file
-				if yes
-				check if the password inputed is mapped to an object
-				if yes
-				check if that object matches the login name
-				if yes we found the right account
-				*/
-				if ((vectorIterator == loginName) && testMap.find(passWord) != testMap.end()) {					
-					if (testMap[passWord]->getLogin() == loginName) {		
-						cout << "Welcome back: " << testMap[passWord]->getName() << endl;
-						menuLoop(testMap[passWord]);
-						break;
-					}
-				}
-				else {
-					//not found
-					cout << "Not Found" << endl;
-					//add return statement to exit for loop
-				}
-			}
-		}
+		case 'a':
+				  getLogin(userNameList, testMap);
 				  break;
 		case 'E':
-		case 'e': {
-			quitProgram = true;
-			return 0;
-		}
-				  break;
+		case 'e': 
+				  quitProgram = true;
+				  return 0;
+		
 		default: cout << "\nThat is an invalid option, try again\n\n";
 		}
 	}
 
-
+	//deallocate point objects in map
 	for (auto itr = testMap.begin(); itr != testMap.end(); itr++)
 	{
 		delete itr->second;
